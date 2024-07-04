c6fea898b08dc837c2f6b6b7ca58bd467e005bc5
Cody <cajackson05@icloud.com>
2024-07-04 13:13:31 +1000

    Adjusted css

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
         const newText = inputText.value.trim();
         if (newText) {
-            myText.innerHTML += "You > " + newText + '<br>';
+            myText.innerHTML += "You > " + newText + '<br><br>';
         }
 
         // Hard Coded Outputs :(
         if (newText == "How much should I have in my savings?") {
-            myText.innerHTML += "Bot > Based on your weekly essential spending including your rental payments, food spending, fitness costs, etc. A 6 month emergency fund for you would be $9400!" + '<br>';
+            myText.innerHTML += "Bot > Based on your weekly essential spending including your rental payments, food spending, fitness costs, etc. A 6 month emergency fund for you would be $9400!" + '<br><br>';
         } else if (newText == "What is a credit score?") {
-            myText.innerHTML += "Bot > A credit score is a numerical representation of an individual's creditworthiness, ranging from 300 to 850, used by lenders to assess repayment likelihood. It is influenced by payment history, credit utilization, credit history length, types of credit, and recent inquiries. Higher scores lead to better loan terms and can affect rental agreements and employment. Improving a credit score involves timely payments, reducing debt, avoiding excessive credit applications, and monitoring credit reports for errors." + '<br>';
+            myText.innerHTML += "Bot > A credit score is a numerical representation of an individual's creditworthiness, ranging from 300 to 850, used by lenders to assess repayment likelihood. It is influenced by payment history, credit utilization, credit history length, types of credit, and recent inquiries. Higher scores lead to better loan terms and can affect rental agreements and employment. Improving a credit score involves timely payments, reducing debt, avoiding excessive credit applications, and monitoring credit reports for errors." + '<br><br>';
+        } else if (newText == "What is an emergency fund?") {
+            myText.innerHTML += "Bot > An emergency fund is a savings account set aside for unexpected expenses or financial emergencies, such as medical bills, car repairs, or job loss. It provides a financial safety net, helping to avoid debt and maintain stability during unforeseen events." + "<br><br>";
         }
 
         inputText.value = '';
