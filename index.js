a1a62bc48a6a17a153d1ceef6a015981b903b8d5
Cody <cajackson05@icloud.com>
2024-07-04 12:41:13 +1000

    Hard Coded Example Questions

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
     sendButton.addEventListener('click', function(event) {
         event.preventDefault();
 
-        const newText = "You > " + inputText.value.trim();
+        const newText = inputText.value.trim();
         if (newText) {
-            myText.innerHTML += newText + '<br>';
+            myText.innerHTML += "You > " + newText + '<br>';
         }
 
         // Hard Coded Outputs :(
+        if (newText == "How much should I have in my savings?") {
+            myText.innerHTML += "Bot > Based on your weekly essential spending including your rental payments, food spending, fitness costs, etc. A 6 month emergency fund for you would be $9400!" + '<br>';
+        } else if (newText == "What is a credit score?") {
+            myText.innerHTML += "Bot > A credit score is a numerical representation of an individual's creditworthiness, ranging from 300 to 850, used by lenders to assess repayment likelihood. It is influenced by payment history, credit utilization, credit history length, types of credit, and recent inquiries. Higher scores lead to better loan terms and can affect rental agreements and employment. Improving a credit score involves timely payments, reducing debt, avoiding excessive credit applications, and monitoring credit reports for errors." + '<br>';
+        }
 
         inputText.value = '';
     });
