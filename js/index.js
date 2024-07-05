5b7f3eff639f1747ada3004c913050c40451d3a4
Kyl <kylleviwrye@gmail.com>
2024-07-05 12:15:09 +1000

    fix margin

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function() {
             myText.innerHTML += "Bot > A credit score is a numerical representation of an individual's creditworthiness, ranging from 300 to 850, used by lenders to assess repayment likelihood. It is influenced by payment history, credit utilization, credit history length, types of credit, and recent inquiries. Higher scores lead to better loan terms and can affect rental agreements and employment. Improving a credit score involves timely payments, reducing debt, avoiding excessive credit applications, and monitoring credit reports for errors." + '<br><br>';
         } else if (newText == "What is an emergency fund?") {
             myText.innerHTML += "Bot > An emergency fund is a savings account set aside for unexpected expenses or financial emergencies, such as medical bills, car repairs, or job loss. It provides a financial safety net, helping to avoid debt and maintain stability during unforeseen events." + "<br><br>";
+        } else if (newText == "How long will it take to save up for my vacation based on my income?") {
+            myText.innerHTML += "Bot > It will take you approximately 4 and a half months based on your current income!" + '<br></br>';
         }
 
         inputText.value = '';
